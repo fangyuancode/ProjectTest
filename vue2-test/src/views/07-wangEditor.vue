a97a0c152fe1c13a51510d78d3c490e8ef904fc2
fangyuancode <3258552734@qq.com>
2024-08-07 10:07:33 +0800

    fix:修改

--- vue2-test/src/views/07-wangEditor.vue
+++ vue2-test/src/views/07-wangEditor.vue
@@ -1,19 +1,24 @@
 <template>
   <div style="border: 1px solid #ccc;">
-    <Toolbar v-show="showIndex===0" style="border-bottom: 1px solid #ccc" :editor="editor" :defaultConfig="toolbarConfig" :mode="mode" />
 
-    <Toolbar v-show="showIndex===1" style="border-bottom: 1px solid #ccc" :editor="editor1" :defaultConfig="toolbarConfig1" :mode="mode1" />
+    <!-- <div v-for="(item,index) in list" :key="index">
+      <div v-show="showIndex===item.index" style="height:60px;background-color: pink;">
+        <h1>ewew{{ item.index }}</h1>
+        <Toolbar style="border-bottom: 1px solid #ccc" :editor="item['editor'+item.index]" :defaultConfig="item['toolbarConfig'+item.index]" :mode="item['mode'+item.index]" />
+      </div>
+    </div> -->
+    <Toolbar style="border-bottom: 1px solid #ccc" :editor="list[indextag]['editor'+(indextag+1)]" :defaultConfig="list[indextag]['toolbarConfig'+(indextag+1)]" :mode="list[indextag]['mode'+(indextag+1)]" />
+    <!-- <Toolbar style="border-bottom: 1px solid #ccc" :editor="editor" :defaultConfig="toolbarConfig" :mode="mode" /> -->
 
     <div class="content">
+      <div v-for="(item,index) in list" :key="index">
+        <h4>{{item['title'+item.index]}}</h4>
 
-      <h4>编辑一</h4>
-
-      <Editor style="height: 100px; overflow-y: hidden;" v-model="html" :defaultConfig="editorConfig" :mode="mode" @onCreated="onCreated" @onChange="onChange" @onDestroyed="onDestroyed" @onMaxLength="onMaxLength" @onFocus="onFocus" @onBlur="onBlur" @customAlert="customAlert" @customPaste="customPaste" />
+        <Editor style="height: 100px; overflow-y: hidden;" v-model="item['html'+item.index]" :defaultConfig="item['defaultConfig'+item.index]" :mode="item['mode'+item.index]" @onCreated="onCreated(item,$event)" @onChange="onChange" @onDestroyed="onDestroyed" @onMaxLength="onMaxLength" @onFocus="onFocus(item,editor)" @onBlur="onBlur" @customAlert="customAlert" @customPaste="customPaste" />
+      </div>
 
     </div>
-    <h4>编辑二</h4>
 
-    <Editor style="height: 100px; overflow-y: hidden;" v-model="html1" :defaultConfig="editorConfig1" :mode="mode1" @onCreated="onCreated1" @onChange="onChange1" @onDestroyed="onDestroyed1" @onMaxLength="onMaxLength1" @onFocus="onFocus1" @onBlur="onBlur1" @customAlert="customAlert1" @customPaste="customPaste1" />
   </div>
 </template>
 <script>
@@ -34,14 +39,60 @@ export default {
       toolbarConfig1: {},
       editorConfig1: { placeholder: "请输入内容..." },
       mode1: "default", // or 'simple'
-      showIndex: 0,
+      showIndex: 1,
+      // 数据
+      list: [
+        {
+          index: 1,
+          title1: "标题一",
+          editor1: "",
+          html1: "",
+          editorConfig1: { placeholder: "请输入内容..." },
+          mode1: "default", // or 'simple'
+          toolbarConfig1: {},
+        },
+        {
+          index: 2,
+          title2: "标题二",
+          editor2: "",
+          html2: "",
+          editorConfig2: { placeholder: "请输入内容..." },
+          mode2: "default", // or 'simple'
+          toolbarConfig2: {},
+        },
+        {
+          index: 3,
+          title3: "标题三",
+          editor3: "",
+          html3: "",
+          editorConfig3: { placeholder: "请输入内容..." },
+          mode3: "default", // or 'simple'
+          toolbarConfig3: {},
+        },
+      ],
+      indextag: 1,
     };
   },
+  computed: {},
   methods: {
-    onCreated(editor) {
-      this.editor = Object.seal(editor);
-      console.log("onCreated", editor);
+    onCreated(item, $event) {
+      console.log("onCreated", $event);
+      let falg = false;
+      if (falg) {
+        this.indextag = item.index - 1; //list元素的下标
+      } else {
+        falg = true;
+      }
+
+      console.log("indextag", this.indextag);
+      this.list[item.index - 1]["editor" + item.index] = Object.seal($event); // 一定要用 Object.seal() ，否则会报错
+      // console.log("onCreated", editor);
+      console.log("object", this.list);
     },
+    // onCreated(editor) {
+    //   this.editor = Object.seal(editor);
+    //   console.log("onCreated", editor);
+    // },
     onChange(editor) {
       console.log("onChange", editor.children);
     },
@@ -51,9 +102,15 @@ export default {
     onMaxLength(editor) {
       console.log("onMaxLength", editor);
     },
-    onFocus(editor) {
+    onFocus(item, editor) {
+      console.log("item", item);
+
+      this.indextag = item.index - 1; //list元素的下标
+      let indextag = this.indextag;
+      console.log("ww", this.list[indextag]["editor" + (indextag + 1)]);
+
       console.log("onFocus", editor);
-      this.showIndex = 0;
+      this.showIndex = item.index;
     },
     onBlur(editor) {
       console.log("onBlur", editor);
@@ -77,50 +134,6 @@ export default {
       // 返回 true ，继续默认的粘贴行为
       // callback(true)
     },
-
-    // 分割
-    onCreated1(editor) {
-      this.editor1 = Object.seal(editor); // 一定要用 Object.seal() ，否则会报错
-    },
-    onCreated1(editor) {
-      this.editor1 = Object.seal(editor);
-      console.log("onCreated", editor);
-    },
-    onChange1(editor) {
-      console.log("onChange", editor.children);
-    },
-    onDestroyed1(editor) {
-      console.log("onDestroyed", editor);
-    },
-    onMaxLength1(editor) {
-      console.log("onMaxLength", editor);
-    },
-    onFocus1(editor) {
-      console.log("onFocus1", editor);
-      this.showIndex = 1;
-    },
-    onBlur1(editor) {
-      console.log("onBlur", editor);
-    },
-    customAlert1(info, type) {
-      window.alert(`customAlert in Vue demo\n${type}:\n${info}`);
-    },
-    customPaste1(editor, event, callback) {
-      console.log("ClipboardEvent 粘贴事件对象", event);
-      // const html = event.clipboardData.getData('text/html') // 获取粘贴的 html
-      // const text = event.clipboardData.getData('text/plain') // 获取粘贴的纯文本
-      // const rtf = event.clipboardData.getData('text/rtf') // 获取 rtf 数据（如从 word wsp 复制粘贴）
-
-      // 自定义插入内容
-      editor.insertText("xxx");
-
-      // 返回 false ，阻止默认粘贴行为
-      event.preventDefault();
-      callback(false); // 返回值（注意，vue 事件的返回值，不能用 return）
-
-      // 返回 true ，继续默认的粘贴行为
-      // callback(true)
-    },
   },
   mounted() {
     // 模拟 ajax 请求，异步渲染编辑器
@@ -129,12 +142,10 @@ export default {
     // }, 1500);
   },
   beforeDestroy() {
-    const editor = this.editor;
+    console.log("beforeDestroy", this.list[this.indextag]);
+    const editor = this.list[this.indextag]["editor" + (this.indextag + 1)];
     if (editor == null) return;
     editor.destroy(); // 组件销毁时，及时销毁编辑器
-    const editor1 = this.editor1;
-    if (editor1 == null) return;
-    editor1.destroy(); // 组件销毁时，及时销毁编辑器
   },
 };
 </script>
